8290359bdd513624e3618c49c0796bc6e6038bb3
hari0409 <73524123+hari0409@users.noreply.github.com>
2021-05-04 11:00:07 +0530

    Create approximator.py

---
+++ approximator.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def approx(a,b):
+    return np.isclose(a, b, rtol=0.01, atol=0.001, equal_nan=False)
+
+def predict(m1,m2):
+    print("calculating Tolerance")
+    t=0
+    f=0
+    total=0
+    for i in m1:
+        for j in m2:
+            ans=approx(i,j)
+            if ans==True:
+                t+=1
+                total+=1
+            elif ans==False:
+                f+=1
+                total+=1
+    print(t)
+    print(f)
+    print(total)
+    return (t/total)*100
