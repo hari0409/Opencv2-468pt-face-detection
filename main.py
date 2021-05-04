d72baffa2d6fcd03ed1dbb40c4aede79d41fcb3d
hari0409 <73524123+hari0409@users.noreply.github.com>
2021-05-04 19:57:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from photo468 import photo
 from definer import definer
 from approxiator import predict
+import time
 
+start=time.time()
 # Tolerance that can be accepted
 tol=float(input("tolerance"))
 
 # Giving the image to process and give all the slopes
-m1=photo("Resources/scanned/k1.jpg",n=10)
-m2=photo("Resources/scanned/face2.jpg",n=10)
+m1=photo("Resources/scanned/a1.jpg",n=10)
+m2=photo("Resources/scanned/a2.jpg",n=10)
 
 # Predict the value of Tolerance
 x=predict(m1,m2)
@@ -15,3 +17,5 @@ print("Found Tolerance==",x)
 
 # Print the Result
 definer(x,tol)
+end=time.time()
+print("Runtime:",end-start)
