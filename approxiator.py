97013e2bc1ad6cbddf9ed0a097e27c8e0a29f156
hari0409 <73524123+hari0409@users.noreply.github.com>
2021-05-05 19:47:18 +0530

    Add files via upload

---
+++ approxiator.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def approx(a,b):
+    return np.isclose(a, b, rtol=0.01, atol=0.01, equal_nan=False)
+
+def predict(m1,m2):
+    print("Calculating Tolerance")
+    print("Calculation of Tolerance may take some time!!")
+    t=0
+    f=0
+    total=0
+    for i in m1:
+        for j in m2:
+            ans=approx(i,j)
+            if ans==True:
+                t+=1
+                total+=1
+            elif ans==False:
+                f+=1
+                total+=1
+    print(t)
+    print(f)
+    print(total)
+    x=(t/total)*100
+    print(x)
+    return x
